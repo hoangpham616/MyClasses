75b9b42a42f9e44a3a2bf6d9f290d63a1ee1e0ec
hoangpham616 <hoangpham61691@gmail.com>
2018-03-16 09:41:15 +0700

    * update MyToolEditor.cs

--- Scripts/Editor/MyToolEditor.cs
+++ Scripts/Editor/MyToolEditor.cs
@@ -1,7 +1,7 @@
 /*
  * Copyright (c) 2016 Phạm Minh Hoàng
  * Framework:   MyClasses
- * Class:       MyToolEditor (version 1.3)
+ * Class:       MyToolEditor (version 1.4)
  */
 
 using UnityEditor;
@@ -13,10 +13,10 @@ namespace MyClasses.Tool
     public class MyToolEditor
     {
         /// <summary>
-        /// Delete all PlayerPrefs keys.
+        /// Delete all PlayerPrefs.
         /// </summary>
-        [MenuItem("MyClasses/Utilities/Clear All PlayerPrefs")]
-        public static void ClearAllPlayerPrefs()
+        [MenuItem("MyClasses/Utilities/Clear PlayerPrefs", false, 1)]
+        public static void ClearPlayerPrefs()
         {
             PlayerPrefs.DeleteAll();
 
@@ -24,9 +24,45 @@ namespace MyClasses.Tool
         }
 
         /// <summary>
+        /// Clear cached AssetBundles.
+        /// </summary>
+        [MenuItem("MyClasses/Utilities/Clear AssetBundles", false, 2)]
+        public static void ClearAssetBundles()
+        {
+            Caching.ClearCache();
+
+            Debug.Log("[MyClasses] AssetBundles was cleared.");
+        }
+
+        /// <summary>
+        /// Check Packing Tag and Asset Bundle.
+        /// </summary>
+        [MenuItem("MyClasses/Utilities/Check Sprite (Packing Tag and Asset Bundle)", false, 13)]
+        public static void CheckSpriteTagsAndBundles()
+        {
+            Dictionary<string, string> spriteDict = new Dictionary<string, string>();
+            string[] spriteNames = AssetDatabase.FindAssets("t:sprite");
+            foreach (string spriteName in spriteNames)
+            {
+                string spritePath = AssetDatabase.GUIDToAssetPath(spriteName);
+                TextureImporter textureImporter = TextureImporter.GetAtPath(spritePath) as TextureImporter;
+                if (!spriteDict.ContainsKey(textureImporter.spritePackingTag))
+                {
+                    spriteDict.Add(textureImporter.spritePackingTag, textureImporter.assetBundleName);
+                }
+                else if (spriteDict[textureImporter.spritePackingTag] != textureImporter.assetBundleName)
+                {
+                    Debug.LogError("[MyClasses] Sprite \"" + textureImporter.assetPath + "\" (PackingTag=\"" + textureImporter.spritePackingTag + "\") should be packed in Asset Bundle \"" + spriteDict[textureImporter.spritePackingTag] + "\".");
+                }
+            }
+
+            Debug.Log("[MyClasses] Sprite checking completed.");
+        }
+
+        /// <summary>
         /// Create a noise texture.
         /// </summary>
-        [MenuItem("MyClasses/Utilities/Create Noise Texture (32x32)")]
+        [MenuItem("MyClasses/Utilities/Create Noise Texture (32x32)", false, 24)]
         public static void CreateNoiseTexture32()
         {
             Texture2D noiseTexture = new Texture2D(32, 32, TextureFormat.ARGB32, false);
@@ -55,7 +91,7 @@ namespace MyClasses.Tool
         /// <summary>
         /// Create a noise texture.
         /// </summary>
-        [MenuItem("MyClasses/Utilities/Create Noise Texture (128x128)")]
+        [MenuItem("MyClasses/Utilities/Create Noise Texture (128x128)", false, 25)]
         public static void CreateNoiseTexture128()
         {
             Texture2D noiseTexture = new Texture2D(128, 128, TextureFormat.ARGB32, false);
@@ -79,28 +115,6 @@ namespace MyClasses.Tool
             System.IO.File.WriteAllBytes("Assets/tex_noise.png", noiseTexture.EncodeToPNG());
 
             Debug.Log("[MyClasses] Noise Texture (128x128) was created.");
-        }
-
-        [MenuItem("MyClasses/Utilities/Check Sprite (Packing Tag and Asset Bundle)")]
-        public static void CheckSpriteTagsAndBundles()
-        {
-            Dictionary<string, string> spriteDict = new Dictionary<string, string>();
-            string[] spriteNames = AssetDatabase.FindAssets("t:sprite");
-            foreach (string spriteName in spriteNames)
-            {
-                string spritePath = AssetDatabase.GUIDToAssetPath(spriteName);
-                TextureImporter textureImporter = TextureImporter.GetAtPath(spritePath) as TextureImporter;
-                if (!spriteDict.ContainsKey(textureImporter.spritePackingTag))
-                {
-                    spriteDict.Add(textureImporter.spritePackingTag, textureImporter.assetBundleName);
-                }
-                else if (spriteDict[textureImporter.spritePackingTag] != textureImporter.assetBundleName)
-                {
-                    Debug.LogError("[MyClasses] Sprite \"" + textureImporter.assetPath + "\" (PackingTag=\"" + textureImporter.spritePackingTag + "\") should be packed in Asset Bundle \"" + spriteDict[textureImporter.spritePackingTag] + "\".");
-                }
-            }
-
-            Debug.Log("[MyClasses] Sprite checking completed.");
         }
     }
 }
