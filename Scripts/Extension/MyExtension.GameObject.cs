3dba580f9180ff235810b5172edc0fcd56cbb290
hoangpham616 <hoangpham61691@gmail.com>
2018-04-13 17:39:25 +0700

    * update MyExtension.GameObject.cs

--- Scripts/Extension/MyExtension.GameObject.cs
+++ Scripts/Extension/MyExtension.GameObject.cs
@@ -1,7 +1,7 @@
-﻿/*
+/*
  * Copyright (c) 2016 Phạm Minh Hoàng
  * Framework:   MyClasses
- * Class:       MyExtension.GameObject (version 1.0)
+ * Class:       MyExtension.GameObject (version 1.1)
  */
 
 using UnityEngine;
@@ -9,26 +9,44 @@ using UnityEngine;
 namespace MyClasses
 {
     public static partial class MyExtension
-	{
-		/// <summary>
-		/// Return the first child of the specified object.
-		/// </summary>
-		public static GameObject GetFirstChild(this GameObject obj)
-		{
-			if (obj != null)
-			{
-				int countChild = obj.transform.childCount;
-				if (countChild > 0)
-				{
-					return obj.transform.GetChild(0).gameObject;
-				}
-			}
-			return null;
-		}
+    {
+        /// <summary>
+        /// Add a component into specified game object if the component doesn't exist on this game object.
+        /// </summary>
+        public static T GetOrAddComponent<T>(this GameObject obj) where T : Component
+        {
+            if (obj != null)
+            {
+                T component = obj.GetComponent<T>();
+                if (component == null)
+                {
+                    component = obj.AddComponent<T>();
+                }
+                return component;
+            }
 
-		/// <summary>
-		/// Return the last child of the specified object.
-		/// </summary>
+            return null;
+        }
+
+        /// <summary>
+        /// Return the first child of the specified object.
+        /// </summary>
+        public static GameObject GetFirstChild(this GameObject obj)
+        {
+            if (obj != null)
+            {
+                int countChild = obj.transform.childCount;
+                if (countChild > 0)
+                {
+                    return obj.transform.GetChild(0).gameObject;
+                }
+            }
+            return null;
+        }
+
+        /// <summary>
+        /// Return the last child of the specified object.
+        /// </summary>
         public static GameObject GetLastChild(this GameObject obj)
         {
             if (obj != null)
